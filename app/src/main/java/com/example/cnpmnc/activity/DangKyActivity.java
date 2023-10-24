a3d38b4254ee850b899f278b18c96989dd29baac
DoanVinhThanh <doanvinhthanhcc@gmail.com>
2023-10-24 17:06:33 +0700

    Update dang ki dang nhap

--- app/src/main/java/com/example/cnpmnc/activity/DangKyActivity.java
+++ app/src/main/java/com/example/cnpmnc/activity/DangKyActivity.java
@@ -6,19 +6,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cnpmnc.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
 public class DangKyActivity extends AppCompatActivity {
 
-    private EditText edtEmail,edtPass,edtRepass,edtHoten,edtNgaySinh,edtSDT,edtQuocTich,;
+    private EditText edtEmail,edtPass,edtRepass,edtHoten,edtNgaySinh,edtSDT,edtQuocTich;
     private Button BtnDangKy;
 
     private TextView Layout_Dang_Ky;
+    StorageReference storageReference;
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,34 +49,7 @@ public class DangKyActivity extends AppCompatActivity {
 //        edtQuocTich = findViewById(R.id);
     }
     private void initListener(){
-        BtnDangKy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String strEmail = edtEmail.getText().toString().trim();
-                String strPass = edtPass.getText().toString().trim();
 
-                FirebaseAuth auth = FirebaseAuth.getInstance();
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                    updateUI(null);
-                                }
-                            }
-                        });
-
-            }
-        });
         Layout_Dang_Ky.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,5 +57,41 @@ public class DangKyActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        BtnDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strEmail = edtEmail.getText().toString().trim();
+                String strPass = edtPass.getText().toString().trim();
+                String strRePass = edtRepass.getText().toString().trim();
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+                if (!strPass.equals(strRePass)) {
+                    // Passwords do not match
+                    Toast.makeText(DangKyActivity.this, "Mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.createUserWithEmailAndPassword(strEmail, strPass)
+                            .addOnCompleteListener(DangKyActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Intent intent = new Intent(DangKyActivity.this, HomePageActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(DangKyActivity.this, "Tạo tài khoản thất bại", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+
+
+
+
     }
-}
+
+    }
--- app/src/main/java/com/example/cnpmnc/activity/DangNhapActivity.java
+++ app/src/main/java/com/example/cnpmnc/activity/DangNhapActivity.java
@@ -1,19 +1,31 @@
 package com.example.cnpmnc.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cnpmnc.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DangNhapActivity extends AppCompatActivity {
 
+    private ProgressDialog progressDialog;
     private TextView Layout_Dang_Ky;
+    private EditText edtEmailDangNhap,edtPassDangNhap;
+    Button BtnDangNhap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,7 +35,13 @@ public class DangNhapActivity extends AppCompatActivity {
         initListener();
     }
     private void initUI(){
+
         Layout_Dang_Ky = findViewById(R.id.layout_dangky);
+
+        edtEmailDangNhap = findViewById(R.id.edtEmailDangNhap);
+        edtPassDangNhap= findViewById(R.id.edtPassDangNhap);
+        BtnDangNhap = findViewById(R.id.btnDangNhap);
+        progressDialog = new ProgressDialog(this);
     }
     private void initListener(){
 
@@ -32,6 +50,33 @@ public class DangNhapActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(DangNhapActivity.this,DangKyActivity.class);
                 startActivity(intent);
+            }
+        });
+        BtnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strEmail = edtEmailDangNhap.getText().toString().trim();
+                String strPass = edtPassDangNhap.getText().toString().trim();
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                progressDialog.show();
+
+                mAuth.signInWithEmailAndPassword(strEmail, strPass)
+                        .addOnCompleteListener(DangNhapActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Intent intent = new Intent(DangNhapActivity.this,HomePageActivity.class);
+                                    startActivity(intent);
+                                    finishAffinity();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(DangNhapActivity.this, "Đang nhập thất bại.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
             }
         });
     }
