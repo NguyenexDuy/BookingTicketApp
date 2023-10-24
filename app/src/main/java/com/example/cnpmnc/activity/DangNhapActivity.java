1603da700dbf74740477b3f725387098d883f66f
DoanVinhThanh <doanvinhthanhcc@gmail.com>
2023-10-24 17:11:14 +0700

    rename

--- app/src/main/java/com/example/cnpmnc/activity/DangNhapActivity.java
+++ app/src/main/java/com/example/cnpmnc/activity/DangNhapActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,7 +29,7 @@ public class DangNhapActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_dang_nhap);
         initUI();
         initListener();
     }
