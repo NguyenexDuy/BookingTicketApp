c237bf4a72d5957a53c694f7009289ac284843d4
DoanVinhThanh <doanvinhthanhcc@gmail.com>
2023-10-25 15:20:14 +0700

    Update

--- app/src/main/java/com/example/cnpmnc/activity/DangKyActivity.java
+++ app/src/main/java/com/example/cnpmnc/activity/DangKyActivity.java
@@ -2,6 +2,7 @@ package com.example.cnpmnc.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,11 +15,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cnpmnc.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DangKyActivity extends AppCompatActivity {
 
@@ -26,6 +34,7 @@ public class DangKyActivity extends AppCompatActivity {
     private Button BtnDangKy;
 
     private TextView Layout_Dang_Ky;
+    private String UserId;
     StorageReference storageReference;
     FirebaseFirestore firestore;
     @Override
@@ -42,7 +51,7 @@ public class DangKyActivity extends AppCompatActivity {
         edtPass = findViewById(R.id.edtPassDangKy);
         edtRepass = findViewById(R.id.edtRePassDangKy);
         BtnDangKy = findViewById(R.id.BtnDangKy);
-        Layout_Dang_Ky = findViewById(R.id.layout_dangky);
+        Layout_Dang_Ky = findViewById(R.id.layout_dangnhap);
         edtHoten = findViewById(R.id.edtHoTen);
         edtNgaySinh = findViewById(R.id.edtNgaySinh);
         edtSDT = findViewById(R.id.edtSDT);
@@ -63,9 +72,25 @@ public class DangKyActivity extends AppCompatActivity {
                 String strEmail = edtEmail.getText().toString().trim();
                 String strPass = edtPass.getText().toString().trim();
                 String strRePass = edtRepass.getText().toString().trim();
+                String strHoten = edtHoten.getText().toString();
+                String strNgaySinh = edtNgaySinh.getText().toString();
+                String strSDT = edtSDT.getText().toString();
+                String strQuocTich = edtQuocTich.getText().toString();
                 FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-                if (!strPass.equals(strRePass)) {
+                if (strHoten.isEmpty()) {
+                    Toast.makeText(DangKyActivity.this, "Vui lòng điền họ và tên", Toast.LENGTH_SHORT).show();
+
+                } else if (strNgaySinh.isEmpty()) {
+                    Toast.makeText(DangKyActivity.this, "Vui lòng điền ngày sinh", Toast.LENGTH_SHORT).show();
+
+                } else if (strSDT.isEmpty()) {
+                    Toast.makeText(DangKyActivity.this, "Vui lòng điền số điện thoại", Toast.LENGTH_SHORT).show();
+
+                } else if (strQuocTich.isEmpty()) {
+                    Toast.makeText(DangKyActivity.this, "Vui lòng điền quốc tịch", Toast.LENGTH_SHORT).show();
+
+                } else if (!strPass.equals(strRePass)) {
                     // Passwords do not match
                     Toast.makeText(DangKyActivity.this, "Mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
                 } else {
@@ -75,23 +100,35 @@ public class DangKyActivity extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
 
+//                                        FirebaseUser fuser = mAuth.getCurrentUser();
+//                                        fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+//                                            @Override
+//                                            public void onSuccess(Void unused) {
+//                                                Toast.makeText(DangKyActivity.this,"Verification has been sent", Toast.LENGTH_SHORT).show();
+//
+//                                            }
+//                                        }).addOnFailureListener(new OnFailureListener() {
+//                                            @Override
+//                                            public void onFailure(@NonNull Exception e) {
+//                                                Toast.makeText(DangKyActivity.this,"Email not sent", Toast.LENGTH_SHORT).show();
+//
+//                                            }
+//                                        });
+                                        Toast.makeText(DangKyActivity.this,"Đăng ký tài khoản thành công", Toast.LENGTH_SHORT).show();
+//
+//
+//                                        UserId = mAuth.getCurrentUser().getUid();
+
                                         // Sign in success, update UI with the signed-in user's information
                                         Intent intent = new Intent(DangKyActivity.this, HomePageActivity.class);
                                         startActivity(intent);
+                                        finishAffinity();
                                     } else {
                                         // If sign in fails, display a message to the user.
                                         Toast.makeText(DangKyActivity.this, "Tạo tài khoản thất bại", Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
-                }
-            }
-        });
-
-
-
-
-
-    }
-
-    }
+                   }
+            }});
+    }  }
