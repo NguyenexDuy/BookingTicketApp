29e2a28d387cd2e5f594cb2deb1b03103c973670
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2023-10-25 15:24:47 +0700

    chucNangSearch

--- app/src/main/java/com/example/cnpmnc/model/Firebase.java
+++ app/src/main/java/com/example/cnpmnc/model/Firebase.java
@@ -7,11 +7,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -87,4 +92,6 @@ public class Firebase {
                     }
                 });
     }
+
+
 }
