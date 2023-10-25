8ad27e524ef96967c305758ccf61ae097a1ba750
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2023-10-25 16:17:27 +0700

    Revert "Revert "Squashed commit of the following:""
    This reverts commit 0cef9fb7dec7b6c08ebc24f5ce1d33777f4df0ff.

--- app/src/main/java/com/example/cnpmnc/fragment/LoaiVePhoThongFragment.java
+++ app/src/main/java/com/example/cnpmnc/fragment/LoaiVePhoThongFragment.java
@@ -2,13 +2,22 @@ package com.example.cnpmnc.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cnpmnc.R;
+import com.example.cnpmnc.adapter.ChuyenBayAdapter;
+import com.example.cnpmnc.model.ChuyenVeTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,6 +48,10 @@ public class LoaiVePhoThongFragment extends Fragment {
      * @return A new instance of fragment LoaiVePhoThongFragment.
      */
     // TODO: Rename and change types and number of parameters
+
+    private RecyclerView rvVePhoThong;
+    private ChuyenBayAdapter chuyenBayAdapter;
+    private List<ChuyenVeTest> chuyenVeTests;
     public static LoaiVePhoThongFragment newInstance(String param1, String param2) {
         LoaiVePhoThongFragment fragment = new LoaiVePhoThongFragment();
         Bundle args = new Bundle();
@@ -62,5 +75,23 @@ public class LoaiVePhoThongFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_loai_ve_pho_thong, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvVePhoThong = view.findViewById(R.id.rvVePhoThong);
+        rvVePhoThong.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        chuyenVeTests = initData(); // Khởi tạo dữ liệu
+        chuyenBayAdapter = new ChuyenBayAdapter(chuyenVeTests);
+        rvVePhoThong.setAdapter(chuyenBayAdapter);
+    }
+    private List<ChuyenVeTest> initData() {
+        List<ChuyenVeTest> data = new ArrayList<>();
+        data.add(new ChuyenVeTest("BOEING1307", "13/10/2023", "15/10/2023", "00:20", "14:00", "HAN", "MSP", "22.748.000 VND"));
+        data.add(new ChuyenVeTest("BOEING2307", "13/10/2023", "15/10/2023", "00:20", "14:00", "HAN", "MSP", "22.748.000 VND"));
+        return data;
     }
 }
