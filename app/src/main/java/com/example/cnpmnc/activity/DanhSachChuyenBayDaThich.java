d543f59e7e52e13ab1fbf1b7b0603a2b7d6df8ad
PhatNgo03 <116328171+PhatNgo03@users.noreply.github.com>
2023-11-16 22:07:29 +0700

    update xoa chuyen bay yeu thich
    sdsd

--- app/src/main/java/com/example/cnpmnc/activity/DanhSachChuyenBayDaThich.java
+++ app/src/main/java/com/example/cnpmnc/activity/DanhSachChuyenBayDaThich.java
@@ -35,154 +35,47 @@
 
 
     public class DanhSachChuyenBayDaThich extends AppCompatActivity {
-        private ArrayList<ChuyenBay> danhSachYeuThich = new ArrayList<>();
-        private RecyclerView rvDanhSachYeuThich;
-        private ChuyenBayDaThichAdapter chuyenBayDaThichAdapter;
-//        private RcvCateFlightAdapter danhSachYeuThichAdapter;
-
+        ArrayList<ChuyenBay> chuyenBays;
+        RecyclerView rvDanhSachYeuThich;
+        FirebaseFirestore db;
+        ChuyenBayDaThichAdapter chuyenBayDaThichAdapter;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_danh_sach_chuyen_bay_da_thich);
-
-          rvDanhSachYeuThich = findViewById(R.id.rvDanhSachChuyenBayDaThich);
-            GridLayoutManager layoutManager = new GridLayoutManager(this,2);
-            rvDanhSachYeuThich.setLayoutManager(layoutManager);
-
-            chuyenBayDaThichAdapter = new ChuyenBayDaThichAdapter(danhSachYeuThich,this);
+            rvDanhSachYeuThich = findViewById(R.id.rvDanhSachChuyenBayDaThich);
+            chuyenBays = new ArrayList<>();
+            ChuyenBayDaThich();
+            chuyenBayDaThichAdapter = new ChuyenBayDaThichAdapter(this,chuyenBays);
             rvDanhSachYeuThich.setAdapter(chuyenBayDaThichAdapter);
-
-            loadDanhSachYeuThich();
-            chuyenBayDaThichAdapter.setOnItemLongClickListener(new ChuyenBayDaThichAdapter.OnItemLongClickListener() {
-                @Override
-                public void onItemLongClick(ChuyenBay chuyenBay) {
-                    // Hiển thị thông báo xác nhận xóa ở đây
-                    showConfirmationDialog(chuyenBay);
-                }
-            });
+            LinearLayoutManager layoutManager = new GridLayoutManager(this,2);
+            rvDanhSachYeuThich.setLayoutManager(layoutManager);
         }
-
-        private void loadDanhSachYeuThich() {
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            CollectionReference yeuThichCollection = db.collection("favorites");
-            Query query=yeuThichCollection.whereEqualTo("userID",userID);
-            query.get().addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    danhSachYeuThich.clear();
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        ChuyenBay chuyenBay = document.toObject(ChuyenBay.class);
-                        danhSachYeuThich.add(chuyenBay);
+        private void ChuyenBayDaThich()
+        {
+            db=FirebaseFirestore.getInstance();
+            db.collection("favorites").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                        String IdChuyenBayNe = documentSnapshot.getId();
+                        String DiemDen = (String) documentSnapshot.get("DiemDen");
+                        String DiemDi = (String) documentSnapshot.get("DiemDi");
+                        String GioBatDau = (String) documentSnapshot.get("GioBatDau");
+                        String HinhAnh = (String) documentSnapshot.get("HinhAnh");
+                        String NgayDi = (String) documentSnapshot.get("NgayDi");
+                        String NgayVe = (String) documentSnapshot.get("NgayVe");
+                        String SoLuongGheTrong = (String) documentSnapshot.get("SoLuongGheTrong");
+                        String SoLuongGheVipTrong = (String) documentSnapshot.get("SoLuongGheVipTrong");
+                        String TrangThai = (String) documentSnapshot.get("TrangThai");
+                        String MoTa  = (String) documentSnapshot.get("MoTa");
+                        String MoTaDiemDap = (String) documentSnapshot.get("MoTaDiemDap");
+                        Boolean isYeuThich = (Boolean) documentSnapshot.get("isYeuThich");
+                        ChuyenBay chuyenBay = new ChuyenBay(IdChuyenBayNe,DiemDen,DiemDi,GioBatDau,HinhAnh,NgayDi,NgayVe,SoLuongGheTrong,SoLuongGheVipTrong,TrangThai,MoTa,MoTaDiemDap,isYeuThich);
+                        chuyenBays.add(chuyenBay);
                     }
                     chuyenBayDaThichAdapter.notifyDataSetChanged();
                 }
             });
-    }
-        private void showConfirmationDialog(ChuyenBay chuyenBay) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Xác nhận xóa");
-            builder.setMessage("Bạn có muốn xóa khỏi chuyến bay yêu thích ?");
-            builder.setPositiveButton("Xóa", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-//                   deleteItem(chuyenBay);
-//                    xoaDulieuTrenFirestore(chuyenBay);
-                    deleteItemFromFirestore(chuyenBay);
-
-                }
-            });
-
-            builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
-            AlertDialog dialog = builder.create();
-            dialog.show();
         }
-        private void deleteItem(ChuyenBay chuyenBay) {
-                int position = danhSachYeuThich.indexOf(chuyenBay);
-                if (position != -1) {
-                    danhSachYeuThich.remove(position);
-                    String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                    String itemID = chuyenBay.getIdChuyenBay();
-                    FirebaseFirestore db = FirebaseFirestore.getInstance();
-                    CollectionReference yeuThichCollection = db.collection("favorites");
-                    DocumentReference documentReference = yeuThichCollection.document(userID + "_" + itemID);
-                    documentReference.delete()
-                            .addOnSuccessListener(aVoid -> {
-                                chuyenBayDaThichAdapter.notifyItemRemoved(position);
-                                Toast.makeText(this, "Xóa thành công !!", Toast.LENGTH_SHORT).show();
-                            })
-                            .addOnFailureListener(e -> {
-                                Toast.makeText(this, "Xóa không thành công !!", Toast.LENGTH_SHORT).show();
-                                Log.e("Firestore", "Error deleting document: " + e.getMessage(), e);
-                            });
-                }
-            }
-        private void xoaDulieuTrenFirestore(ChuyenBay chuyenBay) {
-            int position = danhSachYeuThich.indexOf(chuyenBay);
-            if (position != -1) {
-                danhSachYeuThich.remove(position);
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-                // Check if the ID is not null before proceeding
-                String documentID = chuyenBay.getIdChuyenBay();
-                Log.d("Firestore", "Document ID: " + documentID);
-                if (documentID != null) {
-                    CollectionReference yeuThichCollection = db.collection("favorites");
-                    yeuThichCollection
-                            .document(documentID)
-                            .delete()
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Toast.makeText(DanhSachChuyenBayDaThich.this, "Đã xóa khỏi danh sách yêu thích!", Toast.LENGTH_SHORT).show();
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(DanhSachChuyenBayDaThich.this, "Lỗi khi xóa khỏi danh sách yêu thích", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                } else {
-                    // Handle the case where the ID is null
-                    Toast.makeText(DanhSachChuyenBayDaThich.this, "ID is null", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-        private void deleteItemFromFirestore(ChuyenBay chuyenBay) {
-
-            String itemID = chuyenBay.getIdChuyenBay();
-            if (itemID != null) {
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                CollectionReference yeuThichCollection = db.collection("favorites");
-
-                yeuThichCollection.document(itemID)
-                        .delete()
-                        .addOnSuccessListener(aVoid -> {
-                            // Item deleted successfully from Firestore
-                            // Now, update the local list and notify the adapter
-                            int position = danhSachYeuThich.indexOf(chuyenBay);
-                            if (position != -1) {
-                                danhSachYeuThich.remove(position);
-                                chuyenBayDaThichAdapter.notifyItemRemoved(position);
-                                Toast.makeText(this, "Xóa thành công từ Firestore!!", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(e -> {
-                            // Handle failure to delete item from Firestore
-                            Toast.makeText(this, "Xóa không thành công từ Firestore!!", Toast.LENGTH_SHORT).show();
-                            Log.e("Firestore", "Error deleting document: " + e.getMessage(), e);
-                        });
-            } else {
-                // Handle the case where the document ID is null
-                Toast.makeText(this, "Document ID is null", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-
-
     }
--- app/src/main/java/com/example/cnpmnc/adapter/ChuyenBayDaThichAdapter.java
+++ app/src/main/java/com/example/cnpmnc/adapter/ChuyenBayDaThichAdapter.java
@@ -1,5 +1,6 @@
 package com.example.cnpmnc.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,103 +18,89 @@ import com.example.cnpmnc.R;
 import com.example.cnpmnc.activity.ChiTietFlightActivity;
 import com.example.cnpmnc.model.ChuyenBay;
 import com.example.cnpmnc.model.Firebase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class ChuyenBayDaThichAdapter extends RecyclerView.Adapter<ChuyenBayDaThichAdapter.ViewHolder>{
 
-    private ArrayList<ChuyenBay> flightlist;
     private Context context;
-    private Firebase firebase;
     private ArrayList<ChuyenBay> favoriteFlights = new ArrayList<>();
+    ArrayList<ChuyenBay> chuyenBaysLiked;
 
-    public ChuyenBayDaThichAdapter(ArrayList<ChuyenBay> flightlist, Context context) {
-        this.flightlist = flightlist;
+    public ChuyenBayDaThichAdapter(Context context, ArrayList<ChuyenBay> chuyenBaysLiked) {
         this.context = context;
-        if (favoriteFlights == null) {
-            favoriteFlights = new ArrayList<>();
-        }
-    }
-
-    public ChuyenBayDaThichAdapter(Context context, ArrayList<ChuyenBay> flightlist, Firebase firebase) {
-        this.flightlist = flightlist;
-        this.context = context;
-        this.firebase = firebase;
+        this.chuyenBaysLiked = chuyenBaysLiked;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_flightdathich, parent, false);
-        return new ChuyenBayDaThichAdapter.ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_flightdathich,parent,false);
+        ViewHolder chuyenbayYeuThichVH =new ViewHolder(view);
+        return chuyenbayYeuThichVH;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        ChuyenBay currentFlight = flightlist.get(position);
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        ChuyenBay currentFlight = chuyenBaysLiked.get(position);
         holder.priceflight.setText(String.format("%,d", Math.round(100000)) + " VNĐ");
         holder.nameflight.setText(currentFlight.getDiemDen());
         Glide.with(context).load(currentFlight.getHinhAnh()).into(holder.imageflight);
-
         holder.imageflight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(context, "Chuyển qua xem chi tiết", Toast.LENGTH_SHORT).show();
                 Intent intent=new Intent(context, ChiTietFlightActivity.class);
                 intent.putExtra("ThongTinChuyenBay",currentFlight);
                 context.startActivity(intent);
             }
         });
-        holder.tvXoa.setOnLongClickListener(new View.OnLongClickListener() {
+        holder.img_liked.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                if (onItemLongClickListener != null) {
-                    onItemLongClickListener.onItemLongClick(currentFlight);
-                    return true;
-                }
-                return false;
+            public void onClick(View view) {
+                holder.img_liked.setBackgroundResource(R.drawable.heart_24);
+                deleteSachYeuThich(currentFlight ,position);
             }
         });
-
     }
-
     @Override
     public int getItemCount() {
-        return flightlist.size();
+        return chuyenBaysLiked.size();
     }
 
-    public interface OnItemLongClickListener {
-        void onItemLongClick(ChuyenBay chuyenBay);
-    }
 
-    private OnItemLongClickListener onItemLongClickListener;
-
-    public void setOnItemLongClickListener(OnItemLongClickListener listener) {
-        this.onItemLongClickListener = listener;
+    public void deleteSachYeuThich(ChuyenBay chuyenBay,int position)
+    {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docref = db.collection("favorites").document(chuyenBay.getIdChuyenBay());
+        docref.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                chuyenBaysLiked.remove(position);
+                notifyItemRemoved(position);
+                Toast.makeText(context, "Đã xóa khỏi danh sách !!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(context, "Xóa không thành công !!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView nameflight, priceflight,tvXoa;
-        ImageView imageflight;
+        TextView nameflight, priceflight;
+        ImageView imageflight,img_liked;
         public ViewHolder(View itemView) {
             super(itemView);
 
             nameflight = itemView.findViewById(R.id.tv_nameflight);
             priceflight = itemView.findViewById(R.id.tv_priceflight);
             imageflight = itemView.findViewById(R.id.img_rcvflight);
-            tvXoa = itemView.findViewById(R.id.tvXoaItemDathich);
-
-            tvXoa.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    int position = getAdapterPosition();
-                    if (position != RecyclerView.NO_POSITION && onItemLongClickListener != null) {
-                        onItemLongClickListener.onItemLongClick(flightlist.get(position));
-                        return true;
-                    }
-                    return false;
-                }
-            });
+            img_liked = itemView.findViewById(R.id.img_liked);
         }
     }
 }
